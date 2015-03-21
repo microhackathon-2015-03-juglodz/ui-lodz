cc5a7b92c7ac7a6f6aee7b9094260cc30d5dc64c
Radek Antoniuk <radek.antoniuk@quiddia.com>
2015-03-21 16:30:19 +0100

    reporting not needed

--- src/main/groovy/com/ofg/twitter/config/Collaborators.groovy
+++ src/main/groovy/com/ofg/twitter/config/Collaborators.groovy
@@ -7,7 +7,6 @@ class Collaborators {
 
     public static final String CLIENT_SERVICE = 'client-service-lodz'
     public static final String LOAN_SERVICE = 'loan-application-service-lodz'
-    public static final String REPORTING_SERVICE = 'reporting-service-lodz'
     public static final String OFFER_SERVICE = 'marketing-offer-generator-lodz'
     public static final String DECISION_SERVICE = 'loan-application-decision-maker-lodz'
 
