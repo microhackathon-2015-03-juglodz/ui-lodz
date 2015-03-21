ca308a8d456480dfae330ae34f99237fc532e40b
Radek Antoniuk <radek.antoniuk@quiddia.com>
2015-03-21 16:39:12 +0100

    reporting not needed

--- src/main/groovy/com/quiddia/model/Loan.groovy
+++ src/main/groovy/com/quiddia/model/Loan.groovy
@@ -1,9 +1,17 @@
 package com.quiddia.model
 
+import java.util.concurrent.atomic.AtomicInteger
+
 /**
  * Created by warden on 21.03.15.
  */
 class Loan {
     int loanId
     double amount
+    private AtomicInteger currentId = new AtomicInteger(0)
+
+    int getNextLoanId() {
+        return currentId.incrementAndGet().toString()
+    }
+
 }
--- src/main/resources/static/scripts/controllers/footerController.js
+++ src/main/resources/static/scripts/controllers/footerController.js
@@ -4,10 +4,12 @@ angular.module('BootstrapApplication.controllers')
     .controller('FooterCtrl', ['$scope', 'InfoService', function ($scope, InfoService) {
 
         $scope.applicationInfo = {};
-        InfoService.loadApplicationInfo(function(data) {
-            $scope.applicationInfo = {
-                lastCommit: data.app['last-commit'],
-                buildDate: data.app['build-date-time']
-            };
-        });
+        //$scope.data = {}
+        //
+        //InfoService.loadApplicationInfo(function(data) {
+        //    $scope.applicationInfo = {
+        //        lastCommit: data.app['last-commit'],
+        //        buildDate: data.app['build-date-time']
+        //    };
+        //});
     }]);
